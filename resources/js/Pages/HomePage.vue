13396440ff676acf9ba44e5820fa07336a358b56
Eduardo Massarani <edmassarani@gmail.com>
2024-11-24 20:41:45 +0100

    show links in table format

--- resources/js/Pages/HomePage.vue
+++ resources/js/Pages/HomePage.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import CopyIcon from '@/Components/CopyIcon.vue';
 import LinkIcon from '@/Components/LinkIcon.vue';
 import { Head, Link } from '@inertiajs/vue3';
 import axios from 'axios';
@@ -11,9 +12,20 @@ defineProps<{
     phpVersion: string;
 }>();
 
+type Url = {
+    id: number;
+    long_url: string;
+    short_code: string;
+    visit_count: Number;
+    expires_at: Date;
+    updated_at: Date;
+    created_at: Date;
+};
+
 const longUrl = ref('');
-const urlList = ref([]);
+const urlList = ref<Url[]>([]);
 const requestsRemaining = ref(null);
+const navigator = window.navigator;
 
 const submitUrl = () => {
     axios.post('/urls/shorten', { long_url: longUrl.value }).then((res) => {
@@ -86,7 +98,7 @@ onMounted(() => {
 
                         <label
                             for="long-url"
-                            class="flex min-w-[50%] items-center gap-2 rounded-full border py-1 pl-3 pr-1 dark:bg-gray-900"
+                            class="flex min-w-[50%] items-center gap-2 rounded-full border py-1 pl-3 pr-1 dark:bg-gray-800"
                         >
                             <LinkIcon
                                 class="size-5 fill-black dark:fill-white"
@@ -100,7 +112,7 @@ onMounted(() => {
                                 @keydown.enter.prevent="submitUrl"
                             />
                             <button
-                                class="rounded-full bg-gray-200 p-4 font-bold dark:bg-gray-400 dark:text-gray-900"
+                                class="rounded-full bg-gray-200 p-4 font-bold dark:bg-gray-400 dark:text-gray-800"
                                 @click="submitUrl"
                             >
                                 Shorten Now!
@@ -115,8 +127,42 @@ onMounted(() => {
                             more links. Register Now to enjoy unlimited usage
                         </p>
 
-                        <div>
-                            {{ urlList }}
+                        <div
+                            v-if="urlList.length > 0"
+                            class="grid w-full grid-cols-2 overflow-auto rounded-2xl border-transparent"
+                        >
+                            <p class="bg-gray-200 px-6 py-4 dark:bg-gray-800">
+                                Short Link
+                            </p>
+                            <p class="bg-gray-200 px-6 py-4 dark:bg-gray-800">
+                                Original Link
+                            </p>
+                            <template v-for="url in urlList" :key="url.id">
+                                <div
+                                    class="flex items-center gap-4 bg-gray-100 px-6 py-4 dark:bg-gray-950"
+                                >
+                                    <p>
+                                        https://linkly.com/{{ url.short_code }}
+                                    </p>
+                                    <button
+                                        class="rounded-full bg-gray-200 p-2 hover:bg-opacity-70 dark:bg-gray-800"
+                                        @click="
+                                            navigator.clipboard.writeText(
+                                                `https://127.0.0.1/${url.short_code}`,
+                                            )
+                                        "
+                                    >
+                                        <CopyIcon
+                                            class="size-5 fill-black dark:fill-white"
+                                        />
+                                    </button>
+                                </div>
+                                <div
+                                    class="bg-gray-100 px-6 py-4 dark:bg-gray-950"
+                                >
+                                    <p>{{ url.long_url }}</p>
+                                </div>
+                            </template>
                         </div>
                     </div>
                 </main>
